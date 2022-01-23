44a1be4c527de42d08707d8b3dce4a3e85be8c04
JakelinS <jakelinsayel@192.168.0.10>
2022-01-23 16:04:32 +0100

    Parte dos del programa

--- src/EjemploGit1.java
+++ src/EjemploGit1.java
@@ -4,6 +4,7 @@ public class EjemploGit1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Paso 1 del programa");
+		System.out.println("Paso 2 del programa");
 
 	}
 
